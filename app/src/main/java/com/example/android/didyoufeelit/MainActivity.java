7332b4ed0fc04b0a16a83c64c5d16884643cc470
Aggrey Ochieng <aggreyah@gmail.com>
2018-11-08 13:51:12 +0300

    finished working as expected

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -33,12 +34,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new DidYouFeelItBackground().execute(USGS_REQUEST_URL);
+    }
+    /** An Asynch task*/
+    private   class DidYouFeelItBackground extends AsyncTask<String, Void, Event>{
+        @Override
+        protected Event doInBackground(String... urls) {
+            if (urls.length == 0 || urls[0] == null){
+                return null;
+            }
+            Event result = Utils.fetchEarthquakeData(urls[0]);
+            return result;
+        }
 
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
-
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+        @Override
+        protected void onPostExecute(Event result) {
+            if(result == null){
+                return;
+            }
+            updateUi(result);
+        }
     }
 
     /**
